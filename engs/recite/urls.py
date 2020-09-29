57f581e1f20280a87deaf9f9493e57cc0e84002e
python_learning <506909950@qq.com>
2020-09-29 02:57:17 -0700

    bug fix

--- engs/recite/urls.py
+++ engs/recite/urls.py
@@ -20,7 +20,7 @@ from recite.views import *
 urlpatterns = [
     re_path('^login/$', LoginView.as_view(), name='login'),
     re_path('^$', LoginView.as_view(), name='login'),
-    re_path('^logout/$', LoginView.as_view(), name='logout'),
+    re_path('^logout/$', LogoutView.as_view(), name='logout'),
     re_path('^learning/$', Learning.as_view(), name='learning'),
     re_path('^menu/$', Menu.as_view(), name='menu'),
     re_path('^reviewing/$', Reviewing.as_view(), name='reviewing'),
